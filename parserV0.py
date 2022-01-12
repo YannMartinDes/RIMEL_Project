c814daeb72bc6d74e7d407fd166ce100d3dd91ea
TIGRAN06 <tigran.nersissian@etu.unice.fr>
2022-01-12 20:37:58 +0100

    Update parserV0.py

--- parserV0.py
+++ parserV0.py
@@ -1,6 +1,5 @@
 import glob2
-ListFile=glob2.glob(r".\rapidminer-studio-modular-master\rapidminer-studio-core\src\main\java\com\rapidminer\operator\**/*.java")
-def finish(string):
+def findEndIndex(string):
     count=0
     for i in range(len(string)):
         if ((string[i]=='{') or (string[i]=='(')):
@@ -9,18 +8,32 @@ def finish(string):
             count=count-1
         if (count==0 and string[i]==";"):
             return i
-    return -1
-count=0
-for File in ListFile:
-    file1 = open(File, 'r')
-    line = file1.read()
-    if (line.strip().find("addPrecondition") >= 0):
-        print(File)
-        startIndex=line.strip().find("addPrecondition")
-        endString=line[startIndex:len(line)]
-        endIndex=finish(endString)
-        print(line[startIndex:startIndex+endIndex])
-        count=count+1       
-    file1.close()
-print(count)
+    return None
 
+
+ListFile=glob2.glob(r".\rapidminer-studio-modular-master\rapidminer-studio-core\src\main\java\com\rapidminer\operator\**/*.java")
+
+def findPrecondition(ListFile):
+    count=0
+
+    for File in ListFile:
+        file1 = open(File, 'r')
+        body = file1.read()
+        startIndex=body.strip().find(".addPrecondition")
+        
+        if (startIndex>=0):
+            print(File)
+            
+        while (startIndex >= 0):
+            stringToEvaluate=body[startIndex:len(body)]
+            endIndex=findEndIndex(stringToEvaluate)
+            
+            print(body[startIndex:startIndex+endIndex]+"\n\n")
+            count=count+1
+            body=body[startIndex+endIndex:]
+            startIndex=body.strip().find(".addPrecondition")
+        file1.close()
+        
+    print(count)
+
+findPrecondition(ListFile)
