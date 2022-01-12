c450648ef971fc56a781bd211f4fb8448cb72e7b
TIGRAN06 <tigran.nersissian@etu.unice.fr>
2022-01-12 17:44:24 +0100

    fix line mistake

--- parserV0.py
+++ parserV0.py
@@ -1,14 +1,26 @@
 import glob2
 ListFile=glob2.glob(r".\rapidminer-studio-modular-master\rapidminer-studio-core\src\main\java\com\rapidminer\operator\**/*.java")
+def finish(string):
+    count=0
+    for i in range(len(string)):
+        if ((string[i]=='{') or (string[i]=='(')):
+            count=count+1
+        if ((string[i]=='}') or (string[i]==')')):
+            count=count-1
+        if (count==0 and string[i]==";"):
+            return i
+    return -1
+count=0
 for File in ListFile:
     file1 = open(File, 'r')
-    count = 0
-    while True:
-        count += 1
-        line = file1.readline()
-        if (line.strip().find("addPrecondition") >= 0):
-            print(line.strip())
-        if not line:
-            break
+    line = file1.read()
+    if (line.strip().find("addPrecondition") >= 0):
+        print(File)
+        startIndex=line.strip().find("addPrecondition")
+        endString=line[startIndex:len(line)]
+        endIndex=finish(endString)
+        print(line[startIndex:startIndex+endIndex])
+        count=count+1       
     file1.close()
 print(count)
+
