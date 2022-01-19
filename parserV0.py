b66601213b21b8816e3b7513ba67d247f7410015
Valentin Campello <valentin.campello@etu.univ-cotedazur.fr>
2022-01-19 15:02:05 +0100

    Update parserV0.py

--- parserV0.py
+++ parserV0.py
@@ -36,4 +36,43 @@ def findPrecondition(ListFile):
         
     print(count)
 
-findPrecondition(ListFile)
+def preconditionDict(ListFile):
+    count=0
+    dictList = {}
+    for File in ListFile:
+        file1 = open(File, 'r')
+        body = file1.read()
+        startIndex=body.strip().find(".addPrecondition")+21
+        
+            
+        while (startIndex >= 0):
+            stringToEvaluate=body[startIndex:len(body)]
+            endIndex=findNextParenthesis(stringToEvaluate)
+
+            if(endIndex-startIndex<50):
+                #print(body[startIndex:startIndex+endIndex]+"\n\n")
+                if File in dictList:
+                    dictList[File].append(body[startIndex:startIndex+endIndex])
+                else :
+                    dictList[File]=[body[startIndex:startIndex+endIndex]]
+            count=count+1
+            if(endIndex != None):
+                body=body[startIndex+endIndex:]
+            else:
+                body=body[startIndex:]
+            startIndex=body.strip().find(".addPrecondition")
+            if(startIndex > 0):
+                startIndex+=21
+        file1.close()
+    print(count)
+    print(dictList)
+
+def findNextParenthesis(string):
+    count=0
+    for i in range(len(string)):
+        if (string[i]=='('):
+            return i
+    return None    
+
+preconditionDict(ListFile)
+#findPrecondition(ListFile)
